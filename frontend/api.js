77076233fbdbed1bd1bb0d5448b5253100059a5a
Anirudhcs03 <aniruddhan2003@gmail.com>
2026-04-08 15:10:21 +0530

    Connected properly from https frontend to the http backend server

--- frontend/api.js
+++ frontend/api.js
@@ -1,10 +1,16 @@
-﻿(function attachApiClient(windowObject) {
-  const fallbackLocalApi = "http://3.110.81.154";
+(function attachApiClient(windowObject) {
+  const isLocalDevelopment =
+    windowObject.location.hostname === "localhost" ||
+    windowObject.location.hostname === "127.0.0.1";
+  const fallbackLocalApi = "http://localhost:5000";
+  const fallbackProductionApi = windowObject.location.origin;
   const configuredApiBase =
-    windowObject.KANBAN_API_BASE_URL || fallbackLocalApi;
+    windowObject.KANBAN_API_BASE_URL ||
+    (isLocalDevelopment ? fallbackLocalApi : fallbackProductionApi);
 
   async function request(path, options = {}) {
-    const response = await fetch(`${configuredApiBase}${path}`, {
+    const url = new URL(path, configuredApiBase).toString();
+    const response = await fetch(url, {
       headers: {
         "Content-Type": "application/json",
         ...(options.headers || {}),
